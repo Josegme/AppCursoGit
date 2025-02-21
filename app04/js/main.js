8e73d2871d8c26afc65b4ab08cd48a5dd71f5e82
josegme <josegmescobar@gmail.com>
2025-02-21 18:00:00 -0300

    finalizado el proceso de seleccion con js

--- app04/js/main.js
+++ app04/js/main.js
@@ -29,28 +29,53 @@ let nacional = false;
 const PRECIO = 20;
 let estatura = 0;
 
-if(edadPersona <18 ){
-    alert('Es mayor de edad')
-}else{
-    alert('no es mayor de edad')
+function evaluarEdad(){    
+    edadPersona = parseInt(prompt('Por favor, ingresal tu edad:'));
+    if(edadPersona >= 18 ){
+    alert('La persona es mayor de edad')
+    evaluarRequisitosMinimos();
+    }else{
+    alert('La persona no es mayor de edad');
+    return;
+    }
 }
 
-if(edadPersona >= 16 && edadPersona <=40 && peso >= 20){
-    alert('El candiato cumple con los requisitos y es admitido');
-}else{
-    alert('El candidato no cumple con los requisitos minimos');
+function evaluarRequisitosMinimos(){
+    peso = parseFloat(prompt('Dame el peso de la persona: '));
+    if(edadPersona >= 16 && edadPersona <=40 && peso >= 20){
+        alert('El candiato cumple con los requisitos y es admitido');
+        calcularCostoInscripcion();
+        evaluarCategoria();
+    }else{
+        alert('El candidato no cumple con los requisitos minimos');
+        return;
+    }
+    alert('Tu proceso de seleccion ha finalizado con exito.')
 }
 
-if(edadPersona >= 20 && edadPersona <= 30 || nacional === true){
-    let precioConDescuento = 20 - (20* 10/100);
-    alert('El precio que vas a pagar es: ' + precioConDescuento);
-}else{
-    alert('El precio a pagar por ser extranjero: ' + PRECIO);
+function calcularCostoInscripcion(){
+    let respuestaNacionalidad = prompt('Si eres Nacional Respoonde si, de lo contrario responde no.');
+    if(respuestaNacionalidad === 'si'){
+        nacional = true;
+    }else{
+        nacional = false;
+    }
+    if(edadPersona >= 20 && edadPersona <= 30 || nacional === true){
+        let precioConDescuento = 20 - (20* 10/100);
+        alert('El precio que vas a pagar es: ' + precioConDescuento);
+    }else{
+        alert('El precio a pagar por ser extranjero: ' + PRECIO);
+    }
 }
 
-if(estatura != 1.68){
-    alert('Lo sentimos, solo se adminten personas igual a 1.68');
-}else{
-    alert('Tu estatura es la adecuada para ser admitido.')
+function evaluarCategoria(){
+    estatura = parseFloat(prompt('Por favor, ingresa la estatura.'));
+    if(estatura != 1.68){
+        alert('Lo sentimos, solo se adminten personas igual a 1.68');
+    }else{
+        alert('Tu estatura es la adecuada para ser admitido. Felicidades!!')
+    }
 }
+
+
     
